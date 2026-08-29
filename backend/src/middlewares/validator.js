1a67ab85f613135a5b266e5f4c288560940c86ce
Kalab Goitom <kalabgoitommeresa@gmail.com>
2026-08-29 11:49:55 +0300

    added a validateRequest middleware

---
+++ backend/src/middlewares/validator.js
@@ -0,0 +1,17 @@
+function validateRequest(schema) {
+    return (req, res, next) => {
+      const result = schema.safeParse(req.body);
+
+    if (!result.success) {
+      const errorMessages = result.error.issues.map((err) => err.message);
+      const error = errorMessages.join(", ");
+      return res.status(400).json({
+        message: error,
+      });
+    }
+
+    next();
+    }
+}
+
+export default validate;
